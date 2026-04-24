e47585fb381d4d39ddc1286be75593521f5687d0
Niko M <nikolay.moskvin@gmail.com>
2026-04-24 11:57:12 +1000

    Add additional assets for Swagger UI integration

--- lib/grape-swagger-rails/engine.rb
+++ lib/grape-swagger-rails/engine.rb
@@ -10,10 +10,16 @@ module GrapeSwaggerRails
       if app.config.respond_to?(:assets) && defined?(Sprockets)
         sprockets_4_or_later = Gem::Version.new(Sprockets::VERSION) >= Gem::Version.new('4')
 
-        [
-          'grape_swagger_rails/application.js',
-          'grape_swagger_rails/application.css',
-          'grape_swagger_rails/favicon.ico'
+        %w[
+            grape_swagger_rails/application.js
+            grape_swagger_rails/application.css
+            grape_swagger_rails/favicon.ico
+            grape_swagger_rails/swagger-ui.css
+            grape_swagger_rails/index.css
+            grape_swagger_rails/swagger-ui-bundle.js
+            grape_swagger_rails/swagger-ui-standalone-preset.js
+            grape_swagger_rails/favicon-16x16.png
+            grape_swagger_rails/favicon-32x32.png
         ].each do |asset_path|
           app.config.assets.precompile << (sprockets_4_or_later ? asset_path : proc { |path| path == asset_path })
         end
